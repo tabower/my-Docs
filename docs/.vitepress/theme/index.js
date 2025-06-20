e2f12c95f6d74f9402676173f7c1358c6393166a
aa <aa>
2025-06-20 15:25:36 +0800

    .

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,4 +2,15 @@ import Layout from './Layout.vue'
 
 export default {
   Layout,
+  enhanceApp({ app, router }) {
+    router.onAfterRouteChanged = () => {
+      if (typeof window !== 'undefined' && !document.querySelector('script[src*="cloudflareinsights.com"]')) {
+        const script = document.createElement('script')
+        script.defer = true
+        script.src = 'https://static.cloudflareinsights.com/beacon.min.js'
+        script.dataset.cfBeacon = '{"token": "c1eff9ba2dfb4ee1a94d642fb6468ce1"}'
+        document.body.appendChild(script)
+      }
+    }
+  }
 }
