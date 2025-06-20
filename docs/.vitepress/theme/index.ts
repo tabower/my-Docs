2a25940cabac9df6c0baeb07b2be2c1ba4a798c8
aa <aa>
2025-06-20 12:40:24 +0800

    add comm

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import Layout from "./Layout.vue";
+​
+const theme: Theme = {
+  ...DefaultTheme,
+  Layout: Layout,
+};
+​
+export default theme;
