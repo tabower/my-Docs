9a175ff7170b0882ffd1b5fb933b7e269fcb0c92
aa <aa>
2025-06-20 15:14:45 +0800

    .

---
+++ docs/.vitepress/theme/components/Comments.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { title } = useData()
+</script>
+
+<template>
+  <div :key="title" class="giscus">
+    <component
+      :is="'script'"
+      src="https://giscus.app/client.js"
+      data-repo="tabower/my-Docs"
+      data-repo-id="R_kgDOO-GrSw"
+      data-category="General"
+      data-category-id="DIC_kwDOO-GrS84Crv0n"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-lang="en"
+      data-theme="preferred_color_scheme"
+      data-loading="lazy"
+      async
+    />
+  </div>
+</template>
