963680715a8cd58a2436157083d01172947a8575
aa <aa>
2025-06-20 13:38:08 +0800

    gis

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,46 @@
-import Layout from './Layout.vue'
+import DefaultTheme from 'vitepress/theme';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
 
 export default {
-  Layout,
-}
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+    },
+    setup() {
+        // Get frontmatter and route
+        const { frontmatter } = toRefs(useData());
+        const route = useRoute();
+        
+        // Obtain configuration from: https://giscus.app/
+        giscusTalk({
+            repo: 'tabower/my-Docs',
+            repoId: 'R_kgDOO-GrSw',
+            category: 'General', // default: `General`
+            categoryId: 'IC_kwDOO-GrS84Crv0n',
+            mapping: 'pathname', // default: `pathname`
+            inputPosition: 'top', // default: `top`
+            lang: 'zh-CN', // default: `zh-CN`
+            // i18n setting (Note: This configuration will override the default language set by lang)
+            // Configured as an object with key-value pairs inside:
+            // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+            locales: {
+                'zh-Hans': 'zh-CN',
+                'en-US': 'en'
+            },
+            homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
+            lightTheme: 'light', // default: `light`
+            darkTheme: 'transparent_dark', // default: `transparent_dark`
+            // ...
+        }, {
+            frontmatter, route
+        },
+            // Whether to activate the comment area on all pages.
+            // The default is true, which means enabled, this parameter can be ignored;
+            // If it is false, it means it is not enabled.
+            // You can use `comment: true` preface to enable it separately on the page.
+            true
+        );
+    }
+};
